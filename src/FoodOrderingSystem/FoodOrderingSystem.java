72230081bd61b833d5fce64d0996b5884c88fde4
LAB12_USER ON WS22 <L12Y16W22@citulab.net>
2023-11-16 17:24:05 +0800

    Done Simple Calculator

---
+++ src/FoodOrderingSystem/FoodOrderingSystem.java
@@ -0,0 +1,67 @@
+package FoodOrderingSystem;
+
+import javax.swing.*;
+
+public class FoodOrderingSystem extends JFrame{
+    private JPanel jpanel;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cSundae;
+    private JCheckBox cTea;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cPizza;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+
+    public FoodOrderingSystem(){
+        btnOrder.addActionListener(e -> {
+            float total = 0;
+
+            if (cPizza.isSelected()) {
+                total = total + 100;
+            }
+            if (cBurger.isSelected()) {
+                total = total + 80;
+            }
+            if (cFries.isSelected()) {
+                total = total + 65;
+            }
+            if (cSoftDrinks.isSelected()) {
+                total = total + 55;
+            }
+            if (cTea.isSelected()) {
+                total = total + 50;
+            }
+            if (cSundae.isSelected()) {
+                total = total + 40;
+            }
+
+            if(rbNone.isSelected()){
+                total = total;
+            }
+            if(rb5.isSelected()){
+                total = (float) (total - (total * 0.05));
+            }
+            if(rb10.isSelected()){
+                total = (float) (total - (total * 0.10));
+            }
+            if(rb15.isSelected()){
+                total = (float) (total - (total * 0.15));
+            }
+
+            JOptionPane.showMessageDialog(this, "Total price is Php " + String.format("%.2f", total));
+        });
+    }
+    public static void main(String[] args) {
+        FoodOrderingSystem app = new FoodOrderingSystem();
+        app.setContentPane(app.jpanel);
+        app.setSize(600, 500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setTitle("Food Ordering System");
+
+    }
+}
--- src/SimpleCalculator/SimpCal.java
+++ src/SimpleCalculator/SimpCal.java
@@ -4,15 +4,46 @@ import javax.swing.*;
 
 public class SimpCal extends JFrame{
     private JPanel jpanel;
+    private JComboBox cbOperations;
+    private JTextField tfNumber2;
+    private JTextField tfNumber1;
+    private JTextField lblResult;
+    private JButton btnCompute;
 
-    public static void main(String[] args) {
-        SimpCal app = new SimpCal();
-        app.setContentPane(app.jpanel);
-        app.setSize(800, 400);
-        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        app.setVisible(true);
-        app.setTitle("Simple Calculator");
+    public SimpCal(){
+        btnCompute.addActionListener(e ->{
+            try {
+                String operation = (String) cbOperations.getSelectedItem();
+                int number1 = Integer.parseInt(tfNumber1.getText());
+                int number2 = Integer.parseInt(tfNumber2.getText());
+                int result = 0;
 
+                if (operation.equals("+")) {
+                    result = number1 + number2;
+                } else if (operation.equals("-")) {
+                    result = number1 - number2;
+                } else if (operation.equals("*")) {
+                    result = number1 * number2;
+                } else if (operation.equals("/")) {
+                    result = number1 / number2;
+                }
+
+                lblResult.setText(String.valueOf(result));
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Invalid input!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+
+        });
+    }
+        public static void main(String[] args) {
+            SimpCal app = new SimpCal();
+            app.setContentPane(app.jpanel);
+            app.setSize(700, 400);
+            app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            app.setVisible(true);
+            app.setTitle("Simple Calculator");
+
+        }
 
     }
-}
+
